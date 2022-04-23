27c3ba58df0d107693722f273d9d73bd72fb9cfe
wiresboy-exe <liamcbone@gmail.com>
2022-04-23 20:41:41 +0100

    eeeeeeeeeeeeeeeeeeeeeeee

--- updates.js
+++ updates.js
@@ -1,1 +1,39 @@
-console.log('test')
+const fetch = require('node-fetch');
+const fs = require('fs');
+
+const status = require('./info/status.json');
+const sites = require('./info/sites.json');
+
+let testSite = ( site ) => {
+    fetch(site.url).then(data => {
+        let statusres = {
+            online: data.status === 200,
+            code: data.status,
+            text: data.statusText,
+            timestamp: Date.now()
+        }
+
+        if(!status[site.name]){
+            status[site.name] = []
+        }
+
+        status[site.name].push(statusres)
+        fs.writeFileSync('info/status.json', JSON.stringify(status))
+    }).catch(e => {
+        let statusres = {
+            online: false,
+            code: 000,
+            text: 'Fetch Error',
+            timestamp: Date.now()
+        }
+
+        if(!status[site.name]){
+            status[site.name] = []
+        }
+
+        status[site.name].push(statusres)
+        fs.writeFileSync('info/status.json', JSON.stringify(status))
+    })
+}
+
+testSite(sites[0])
